0a8fb56b80c6079a9110eef8352fc2abc104de77
syedaliraza78 <syedaliraza00111@gmail.com>
2025-03-16 08:27:37 +0500

    Search Page

---
+++ client/src/components/FilterPage.tsx
@@ -0,0 +1,52 @@
+import { Button } from "./ui/button";
+import { useRestaurantStore } from "@/store/useRestaurantStore";
+import { Checkbox } from "./ui/checkbox";
+import { Label } from "./ui/label";
+
+export type FilterOptionsState = {
+  id: string;
+  label: string;
+};
+
+const filterOptions: FilterOptionsState[] = [
+  { id: "burger", label: "Burger" },
+  { id: "pizza", label: "Pizza" },
+  { id: "biryani", label: "Biryani" },
+  { id: "momos", label: "Momos" },
+];
+
+export const FilterPage = () => {
+  const { setAppliedFilter, appliedFilter, resetAppliedFilter } =
+    useRestaurantStore();
+
+  const appliedFilterHandler = (value: string) => {
+    setAppliedFilter(value);
+  };
+
+  return (
+    <div className="md:w-72">
+      <div className="flex items-center justify-between">
+        <h1 className="font-medium text-lg">Filter by cuisines</h1>
+        <Button variant="link" onClick={resetAppliedFilter}>
+          Reset
+        </Button>
+      </div>
+
+      {filterOptions.map((option) => (
+        <div key={option.id} className="flex items-center space-x-2 my-5">
+          <Checkbox
+            id={option.id}
+            checked={appliedFilter.includes(option.label)}
+            onChange={() => appliedFilterHandler(option.label)}
+          />
+          <Label
+            htmlFor={option.id}
+            className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+          >
+            {option.label}
+          </Label>
+        </div>
+      ))}
+    </div>
+  );
+};
--- client/src/components/SearchPage.tsx
+++ client/src/components/SearchPage.tsx
@@ -1,10 +1,47 @@
 import { useParams } from "react-router-dom";
+import { FilterPage } from "./FilterPage";
+import { Input } from "./ui/input";
+import { useState } from "react";
+import { useRestaurantStore } from "@/store/useRestaurantStore";
+import { Button } from "./ui/button";
 
 export const SearchPage = () => {
+  const params = useParams();
+  const [searchQuery, setSearchQuery] = useState<string>("");
+  const {
+    loading,
+    searchedRestaurant,
+    searchRestaurant,
+    setAppliedFilter,
+    appliedFilter,
+  } = useRestaurantStore();
+
   const param = useParams();
   return (
-    <div>
-      <p>{param.text}</p>
+    <div className="max-w-7xl mx-auto my-10">
+      <div className="flex flex-col md:flex-row justify-between gap-10">
+        <FilterPage />
+        <div className="flex-1">
+          {/* Search Input Field  */}
+          <div className="flex items-center gap-2">
+            <Input
+              type="text"
+              value={searchQuery}
+              placeholder="Search by restaurant & cuisines"
+              onChange={(e) => setSearchQuery(e.target.value)}
+            />
+            <Button
+              onClick={() =>
+                searchRestaurant(params.text!, searchQuery, appliedFilter)
+              }
+              className="bg-orange hover:bg-hoverOrange"
+            >
+              Search
+            </Button>
+          </div>
+          {/* Searched Items display here  */}
+        </div>
+      </div>
     </div>
   );
 };
