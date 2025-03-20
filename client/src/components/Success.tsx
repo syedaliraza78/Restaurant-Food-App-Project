77513d1f6ed28d044f97ee05f22899f6095658e8
syedaliraza78 <syedaliraza00111@gmail.com>
2025-03-20 22:39:34 +0500

    Resolving Errors

--- client/src/components/Success.tsx
+++ client/src/components/Success.tsx
@@ -20,6 +20,14 @@ export const Success = () => {
   //         </h1>
   //       </div>
   //     );
+  //   if (orders.length === 0)
+  //     return (
+  //       <div className="flex items-center justify-center min-h-screen">
+  //         <h1 className="font-bold text-2xl text-gray-700 dark:text-gray-300">
+  //           Order not found!
+  //         </h1>
+  //       </div>
+  //     );
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-50 dark:bg-gray-900 px-4">
       <div className="bg-white dark:bg-gray-800 shadow-lg rounded-lg p-6 max-w-lg w-full">
