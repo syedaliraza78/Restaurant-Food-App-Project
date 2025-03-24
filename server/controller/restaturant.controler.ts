97b7deb57be5e39b3fd2aaa2f5059d0aa645767c
syedaliraza78 <syedaliraza00111@gmail.com>
2025-03-24 20:05:05 +0500

    Add Resturant Controller Search through filter

--- server/controller/restaturant.controler.ts
+++ server/controller/restaturant.controler.ts
@@ -95,3 +95,111 @@ export const updateRestaurant = async (req: Request, res: Response) => {
 };
 
 //  now we can work of resturant order
+export const getRestaurantOrder = async (req: Request, res: Response) => {
+  try {
+    const restaurant = await Restaurant.findOne({ user: req.id });
+    if (!restaurant) {
+      return res.status(404).json({
+        success: false,
+        message: "Restaurant not found",
+      });
+    }
+
+    const orders = await Order.find({ restaurant: restaurant._id })
+      .populate("restaurant")
+      .populate("user");
+    return res.status(200).json({
+      success: true,
+      orders,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const updateOrderStatus = async (req: Request, res: Response) => {
+  try {
+    const { orderId } = req.params;
+    const { status } = req.body;
+    const order = await Order.findById(orderId);
+    if (!order) {
+      return res.status(404).json({
+        success: false,
+        message: "Order not found",
+      });
+    }
+    order.status = status;
+    await order.save();
+    return res.status(200).json({
+      success: true,
+      status: order.status,
+      message: "Status updated",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const searchRestaurant = async (req: Request, res: Response) => {
+  try {
+    const searchText = req.params.searchText || "";
+    const searchQuery = (req.query.searchQuery as string) || "";
+    const selectedCuisines = ((req.query.selectedCuisines as string) || "")
+      .split(",")
+      .filter((cuisine) => cuisine);
+    const query: any = {};
+    // basic search based on searchText (name ,city, country)
+    console.log(selectedCuisines);
+
+    if (searchText) {
+      query.$or = [
+        { restaurantName: { $regex: searchText, $options: "i" } },
+        { city: { $regex: searchText, $options: "i" } },
+        { country: { $regex: searchText, $options: "i" } },
+      ];
+    }
+    // filter on the basis of searchQuery
+    if (searchQuery) {
+      query.$or = [
+        { restaurantName: { $regex: searchQuery, $options: "i" } },
+        { cuisines: { $regex: searchQuery, $options: "i" } },
+      ];
+    }
+    // console.log(query);
+    // ["momos", "burger"]
+    if (selectedCuisines.length > 0) {
+      query.cuisines = { $in: selectedCuisines };
+    }
+
+    const restaurants = await Restaurant.find(query);
+    return res.status(200).json({
+      success: true,
+      data: restaurants,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const getSingleRestaurant = async (req: Request, res: Response) => {
+  try {
+    const restaurantId = req.params.id;
+    const restaurant = await Restaurant.findById(restaurantId).populate({
+      path: "menus",
+      options: { createdAt: -1 },
+    });
+    if (!restaurant) {
+      return res.status(404).json({
+        success: false,
+        message: "Restaurant not found",
+      });
+    }
+    return res.status(200).json({ success: true, restaurant });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
