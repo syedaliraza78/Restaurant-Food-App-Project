e179a57565f7cbbee2caac3265c99600a7b47898
syedaliraza78 <syedaliraza00111@gmail.com>
2025-03-24 19:50:29 +0500

    Fixing errors

---
+++ server/controller/restaturant.controler.ts
@@ -0,0 +1,97 @@
+import { Request, Response } from "express";
+import { Restaurant } from "../models/restaurant.model.js";
+import { Multer } from "multer";
+import uploadImageOnCloudinary from "../utils/imageUpload.js";
+import { Order } from "../models/order.model";
+
+export const createRestaurant = async (req: Request, res: Response) => {
+  try {
+    const { restaurantName, city, country, deliveryTime, cuisines } = req.body;
+    const file = req.file;
+    const restaurant = await Restaurant.findOne({ user: req.id });
+    if (restaurant) {
+      return res.status(400).json({
+        success: false,
+        message: "Restaurant already exist for this user",
+      });
+    }
+    if (!file) {
+      return res.status(400).json({
+        success: false,
+        message: "Image is required",
+      });
+    }
+    const imageUrl = await uploadImageOnCloudinary(file as Express.Multer.File);
+    await Restaurant.create({
+      user: req.id,
+      restaurantName,
+      city,
+      country,
+      deliveryTime,
+      cuisines: JSON.parse(cuisines),
+      imageUrl,
+    });
+    return res.status(201).json({
+      success: true,
+      message: "Restaurant Added",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+export const getRestaurant = async (req: Request, res: Response) => {
+  try {
+    const restaurant = await Restaurant.findOne({ user: req.id }).populate(
+      "menus"
+    );
+    if (!restaurant) {
+      return res.status(404).json({
+        success: false,
+        restaurant: [],
+        message: "Restaurant not found",
+      });
+    }
+    return res.status(200).json({ success: true, restaurant });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const updateRestaurant = async (req: Request, res: Response) => {
+  try {
+    const { restaurantName, city, country, deliveryTime, cuisines } = req.body;
+    const file = req.file;
+    const restaurant = await Restaurant.findOne({ user: req.id });
+    if (!restaurant) {
+      return res.status(404).json({
+        success: false,
+        message: "Restaurant not found",
+      });
+    }
+    restaurant.restaurantName = restaurantName;
+    restaurant.city = city;
+    restaurant.country = country;
+    restaurant.deliveryTime = deliveryTime;
+    restaurant.cuisines = JSON.parse(cuisines); // in the form of array json
+    // first upload the file on cloudnary then update in db
+    if (file) {
+      const imageUrl = await uploadImageOnCloudinary(
+        file as Express.Multer.File
+      );
+      restaurant.imageUrl = imageUrl;
+    }
+    await restaurant.save();
+    return res.status(200).json({
+      success: true,
+      message: "Restaurant updated",
+      restaurant,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+//  now we can work of resturant order
