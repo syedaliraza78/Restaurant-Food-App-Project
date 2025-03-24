ca688fdd844b7376f2364b1c94db4a897b9b10eb
syedaliraza78 <syedaliraza00111@gmail.com>
2025-03-24 20:23:01 +0500

    Resturant Routes

---
+++ server/routes/resturant.route.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import {
+  createRestaurant,
+  getRestaurant,
+  getRestaurantOrder,
+  getSingleRestaurant,
+  searchRestaurant,
+  updateOrderStatus,
+  updateRestaurant,
+} from "../controller/restaturant.controler.js";
+import upload from "../middleware/multer.js";
+import { isAuthenticated } from "../middleware/isAuthenticated.js";
+
+const router = express.Router();
+
+router
+  .route("/")
+  .post(isAuthenticated, upload.single("imageFile"), createRestaurant);
+router.route("/").get(isAuthenticated, getRestaurant);
+router
+  .route("/")
+  .put(isAuthenticated, upload.single("imageFile"), updateRestaurant);
+router.route("/order").get(isAuthenticated, getRestaurantOrder);
+router.route("/order/:orderId/status").put(isAuthenticated, updateOrderStatus);
+router.route("/search/:searchText").get(isAuthenticated, searchRestaurant);
+router.route("/:id").get(isAuthenticated, getSingleRestaurant);
+
+export default router;
