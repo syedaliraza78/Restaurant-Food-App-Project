7274c843d89da9617114a4caafc20b7c1260a9b9
syedaliraza78 <syedaliraza00111@gmail.com>
2025-04-29 19:24:04 +0500

    Fixing Errors

--- client/src/store/useRestaurantStore.ts
+++ client/src/store/useRestaurantStore.ts
@@ -179,5 +179,9 @@ export const useRestaurantStore = create<RestaurantState>()(
     //   name: "restaurant-name",
     //   storage: createJSONStorage(() => localStorage),
     // }
+    //  {
+    //   name: "restaurant-name",
+    //   storage: createJSONStorage(() => localStorage),
+    // }
   )
 );
